239e4ebd7290450360429c20d3dfa7efa6fba7e7
georgehughes <hughesgeorgem@gmail.com>
2025-12-07 16:51:53 +0100

    First Attempt Day 7 Part 1

---
+++ Solutions/Day7Part1.sh
@@ -0,0 +1,33 @@
+#/bin/bash
+#
+# Usage : Solutions/Day7Part1.sh input.txt
+
+DEBUG=
+
+offsets=
+total=0
+while read line; do
+  next_offsets=$(echo $line| grep "\^" -b -o | grep -o "[0-9]\+")
+  if [[ -z "$next_offsets" ]]; then
+    continue
+  elif [[ -z "$offsets" ]]; then
+    offsets=$next_offsets
+    ((total++))
+    continue
+  fi
+  
+  match_filter_args=$(for v in $offsets; do echo "-e $((v-1)) -e $((v+1))"; done | tr '\n' ' ')
+  non_match_filter_args=$(for v in $next_offsets; do echo "-e $((v-1)) -e $((v+1))"; done | tr '\n' ' ')
+
+  next_offsets_matched=$(echo $next_offsets | grep -w $match_filter_args -o)
+  offsets_non_matched=$(echo $offsets | tr ' ' '\n' | grep -wov $non_match_filter_args)
+  
+  number_matches=$(echo $next_offsets_matched | wc -w)
+  total=$(($total + $number_matches))
+  
+  test $DEBUG && echo Offsets: $offsets, next_offsets: $next_offsets, next_offsets_matched: $next_offsets_matched, offsets_non_matched: $offsets_non_matched, total: $total
+
+  offsets="$next_offsets_matched $offsets_non_matched"
+done < $1
+
+echo $total
