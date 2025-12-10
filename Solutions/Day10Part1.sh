0867efe45bc86aef4b32d2d90c9d6766b6f0eb20
georgehughes <hughesgeorgem@gmail.com>
2025-12-10 20:45:27 +0100

    Prep Day 10

---
+++ Solutions/Day10Part1.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+#
+# Usage : Solutions/Day10Part1.sh input.txt
+
+echo Not Implemented; exit 1
---
+++ Solutions/Day10Part2.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+#
+# Usage : Solutions/Day10Part2.sh input.txt
+
+echo Not Implemented; exit 1
