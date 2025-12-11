71b912a27ec587908d039b785650974d13054564
georgehughes <hughesgeorgem@gmail.com>
2025-12-11 16:19:46 +0100

    Solution Day 10 Part 1

--- Solutions/Day10Part1.sh
+++ Solutions/Day10Part1.sh
@@ -2,40 +2,46 @@
 #
 # Usage : Solutions/Day10Part1.sh input.txt
 
-DEBUG=1
+DEBUG=
 
 QUEUE=.tmp.queue.out
+DEBUG_PATH=.debug.out
 
+> $DEBUG_PATH
+
+total=0
 while read machine; do
 
-  test $DEBUG && echo Machine: $machine
+  test $DEBUG && echo Machine: $machine | tee -a $DEBUG_PATH
 
-  target_state=$(echo $machine | rev | grep -o "[.#]" | grep -n "#" | grep -o [0-9] | awk '{ sum += 2^($1 - 1)}; END { print sum }')
+  target_state=$(echo $machine | grep -o "[.#]" | grep -n "#" | grep -ow "[0-9]\+" | awk '{ sum += 2^($1 - 1)}; END { print sum }')
   buttons=$(echo $machine | grep -o "([0-9,]\+)" | sed 's/[()]//g' | tr ',' ' ' | awk '{ total=0; for (i=1; i<=NF; i++) total+=2^$i; print total}')
 
-  test $DEBUG && echo target_state: $target_state, buttons: $buttons
+  test $DEBUG && echo target_state: $target_state, buttons: $buttons | tee -a $DEBUG_PATH
 
   echo 0 0 > $QUEUE
-  states_visited=
+  states_seen=0
   while [[ "$state" -ne "$target_state" ]]; do
+    if [[ $(wc -l < $QUEUE) -eq 0 ]]; then
+      echo "ERROR: The queue is empty" | tee -a $DEBUG_PATH
+      break
+    fi
     read state count < <(head -n1 $QUEUE)
     sed -i -e '1d' $QUEUE
 
     for button in $buttons; do
       next_state=$(($state ^ $button))
-      next_count=$((count + 1))
+      next_count=$(($count + 1))
 
-      echo $states_visited | grep $next_state || echo $next_state $next_count >> $QUEUE
-      states_visited=$states_visited $next_state
+      echo $states_seen | grep -w $next_state >/dev/null || echo $next_state $next_count >> $QUEUE
+      states_seen="$states_seen $next_state"
     done
-    
-    test $DEBUG && echo state: $state, count: $count, states_visited: $states_visited
-    #state=$target_state
   done
-  cat $QUEUE
+
+  total=$(($total + $count))
 
 done < $1
 
 rm $QUEUE
 
-echo Not Implemented; exit 1
+echo $total
