c3ec335b8c88965bd5d57315bbb8bdb74a87ed93
georgehughes <hughesgeorgem@gmail.com>
2025-12-14 18:54:23 +0100

    Solution Day 5 Part 1

--- Solutions/Day5Part1.sh
+++ Solutions/Day5Part1.sh
@@ -2,4 +2,5 @@
 #
 # Usage : Solutions/Day5Part1.sh input.txt
 
-echo Not Implemented; exit 1
+
+cat $1 | grep "-" | tr '-' ' ' | awk -v numbers_string="$(cat $1 | grep [0-9] | grep -v "-" | tr '\n' ' ')" 'BEGIN { split(numbers_string, numbers, " ") }; { for (n in numbers) if (numbers[n] >= $1 && numbers[n] <= $2) print numbers[n] }' | sort -un | wc -l | grep -o "[0-9]\+"
