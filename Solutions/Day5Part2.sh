9aed57be202b0a54b85255cdf5cd7c3bbd4220d9
georgehughes <hughesgeorgem@gmail.com>
2025-12-15 19:04:24 +0100

    Solution Day 5 Part 2

--- Solutions/Day5Part2.sh
+++ Solutions/Day5Part2.sh
@@ -2,4 +2,27 @@
 #
 # Usage : Solutions/Day5Part2.sh input.txt
 
-cat $1 | grep "-" | tr '-' ' ' | xargs -P0 -n2 seq | sort -nu | wc -l | grep -o "[0-9]\+"
+
+RANGES_FILE=.tmp.ranges.out
+
+touch $RANGES_FILE
+while read lower upper; do
+  found=
+  while read lower2 upper2; do
+    [[ $lower -lt $lower2 ]] && { minLower=$lower; maxLower=$lower2; } || { minLower=$lower2; maxLower=$lower; }
+    [[ $upper -lt $upper2 ]] && { minUpper=$upper; maxUpper=$upper2; } || { minUpper=$upper2; maxUpper=$upper; }
+    
+    
+    if [[ $minUpper -ge $maxLower ]]; then
+      sed -i -e "s/$lower2 $upper2/$minLower $maxUpper/" $RANGES_FILE
+      found=1
+    fi
+  done < <(cat $RANGES_FILE)
+  
+  [[ -z "$found" ]] && echo $lower $upper >> $RANGES_FILE
+  
+done < <(cat $1 | grep "-" | tr '-' ' ' | sort -n)
+
+cat $RANGES_FILE | awk '{ sum += $2 - $1 + 1} ; END {print sum}'
+
+rm $RANGES_FILE
