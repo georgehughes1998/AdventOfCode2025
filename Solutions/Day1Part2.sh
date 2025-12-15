819cec451bcaab1eef73ce1d840238d591b94968
George <43959764+georgehughes1998@users.noreply.github.com>
2025-12-15 13:24:09 +0100

    Add increment variable for seq portability

--- Solutions/Day1Part2.sh
+++ Solutions/Day1Part2.sh
@@ -7,7 +7,8 @@ position=50
 total=0
 while read line; do
   nextPosition=$(($position + $(echo $line | grep -o '[-0-9]\+')))
-  number_zeros=$(seq $position $nextPosition | awk '{ print $1 % 100 } ' | tr ' ' '\n' | sed '1d' | grep -cw 0)
+  [[ $nextPosition -gt $position ]] && increment=1 || increment=-1
+  number_zeros=$(seq $position $increment $nextPosition | awk '{ print $1 % 100 } ' | tr ' ' '\n' | sed '1d' | grep -cw 0)
   total=$(($total + $number_zeros))
   position=$nextPosition
 done < <(cat $1 | sed 's/R//' | sed 's/L/-/')
