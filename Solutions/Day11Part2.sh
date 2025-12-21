76e714dd9af69d0923c96f17c6f23a5ea2bac3f9
georgehughes <hughesgeorgem@gmail.com>
2025-12-21 17:07:58 +0100

    Day 11 Part 2 Solution

--- Solutions/Day11Part2.sh
+++ Solutions/Day11Part2.sh
@@ -3,33 +3,61 @@
 # Usage : Solutions/Day11Part2.sh input.txt
 
 DEBUG=
+DEBUG_LOG=.tmp.day11.log.out
+>$DEBUG_LOG
+
+TMP_MEMOIZE_PATH=.tmp.memoize.out
+>$TMP_MEMOIZE_PATH
 
 graph_path=$1
 
-# Usage : explore_node $node "$visited"
-explore_node() {
-  local visited_grep_args
-  local visited
-  local current_node
+# Function to emulate a map
+# Usage : memoize_set node value
+memoize_set() {
+  [[ $# -ne 3 ]] && { echo ERROR: memoize_set takes 3 arguments; exit 1; }
+  memoize_entryname="_memoize_$1_$2"
+  echo $memoize_entryname $3 >> $TMP_MEMOIZE_PATH
+}
 
-  current_node=$1
-  visited="$2"
-  visited_grep_args=$(echo $visited | tr ' ' '\n' | grep . | awk '{ print "-e "$0 }' | tr '\n' ' ')
-  [[ -n "$visited_grep_args" ]] && visited_grep_args="-v $visited_grep_args" || visited_grep_args="."
+# Function to emulate getting values in a 2D array
+# Usage : memoize_get node
+memoize_get() {
+  [[ $# -ne 2 ]] && { echo ERROR: memoize_get takes 2 arguments; exit 1; }
+  memoize_entryname="_memoize_$1_$2"
+  cat $TMP_MEMOIZE_PATH | grep $memoize_entryname | tail -n1 | awk '{ print $2 }'
+}
+
+# Usage : count_paths $start_node $stop_node $direction
+count_paths() {
+  [[ "$#" -ne 2 ]] && { echo "count_paths requires 2 arguments"; exit 1; }
+  local start_node="$1"
+  local stop_node="$2"
   
-  test $DEBUG && echo current_node: $current_node, visited_grep_args: $visited_grep_args
+  test $DEBUG && echo Looking at start_node: $start_node >> $DEBUG_LOG
   
-  if [[ "$current_node" == "out" ]]; then
-    echo $visited | grep "fft" | grep -q "dac" && echo 1
+  count=$(memoize_get $start_node $stop_node)
+  if [[ "$start_node" == "$stop_node" ]]; then
+  	test $DEBUG && echo $start_node matches stop_node >> $DEBUG_LOG
+    echo 1
+  elif [[ -n "$count" ]]; then
+    test $DEBUG && echo "Read memoized value for $start_node: $count" >> $DEBUG_LOG
+    echo $count
   else
-	  next_nodes=$(cat $graph_path | grep "$current_node:" | sed "s/$current_node://g" | grep $visited_grep_args)
+	  next_nodes=$(cat $graph_path | grep "$start_node:" | sed "s/$start_node://g")
+	  test $DEBUG && echo Expanding next_nodes from $start_node: $next_nodes >> $DEBUG_LOG
 	  
-	  test $DEBUG && echo next_nodes: $next_nodes
+	  count=$(for next_node in $next_nodes; do count_paths $next_node $stop_node; done | awk 'BEGIN { sum=0 }; { sum += $1 }; END { print sum }')
+	  test $DEBUG && echo "Calculated value for $start_node: $count" >> $DEBUG_LOG
 	  
-	  for next_node in $next_nodes; do
-	    explore_node $next_node "$visited $next_node"
-	  done
+	  memoize_set $start_node $stop_node $count
+	  test $DEBUG && echo "Memoized value for $start_node: $(memoize_get $start_node $stop_node)" >> $DEBUG_LOG
+	  echo $count
   fi
 }
 
-explore_node "svr" "" | wc -w | grep -o "[0-9]\+"
+total=$((($(count_paths svr dac) * $(count_paths dac fft) * $(count_paths fft out)) + ($(count_paths svr fft) * $(count_paths fft dac) * $(count_paths dac out)) ))
+
+test $DEBUG && cat $DEBUG_LOG
+echo $total
+ 
+rm $DEBUG_LOG $TMP_MEMOIZE_PATH	
