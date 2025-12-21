ff03f263c2768a0aec27b203928f6c468867fc35
georgehughes <hughesgeorgem@gmail.com>
2025-12-21 10:53:59 +0100

    Copied from Day 11 Part 2, takes too long to run but a shame to remove the (admittedly overly engineered) code which works fine for the first part

--- Solutions/Day11Part1.sh
+++ Solutions/Day11Part1.sh
@@ -6,30 +6,41 @@ DEBUG=
 
 graph_path=$1
 
-# Usage : explore_node $node "$visited"
-explore_node() {
-  local visited_grep_args
-  local visited
-  local current_node
+DIRECTION_FORWARD="forward"
+DIRECTION_BACKWARD="backward"
 
-  current_node=$1
-  visited="$2"
-  visited_grep_args=$(echo $visited | tr ' ' '\n' | grep . | awk '{ print "-e "$0 }' | tr '\n' ' ')
+# Usage : explore_node $start_node $stop_node $direction "$visited"
+explore_node() {
+  [[ "$#" -lt 3 ]] && { echo "$0 requires at least 3 arguments"; exit 1; }
+  local start_node="$1"
+  local stop_node="$2"
+  local direction="$3"
+  local visited="${4:-$1}"
+  
+  local visited_grep_args=$(echo $visited | tr ' ' '\n' | grep . | awk '{ print "-e "$0 }' | tr '\n' ' ')
+
   [[ -n "$visited_grep_args" ]] && visited_grep_args="-v $visited_grep_args" || visited_grep_args="."
   
-  test $DEBUG && echo current_node: $current_node, visited_grep_args: $visited_grep_args
+  test $DEBUG && echo start_node: $start_node, visited_grep_args: $visited_grep_args
   
-  if [[ "$current_node" == "out" ]]; then
-    echo 1
+  if [[ "$start_node" == "$stop_node" ]]; then
+    echo $visited
   else
-	  next_nodes=$(cat $graph_path | grep "$current_node:" | sed "s/$current_node://g" | grep $visited_grep_args)
+    if [[ "$direction" == $DIRECTION_FORWARD ]]; then
+	  	next_nodes=$(cat $graph_path | grep "$start_node:" | sed "s/$start_node://g" | tr ' ' '\n' | grep $visited_grep_args)
+	  elif [[ "$direction" == $DIRECTION_BACKWARD ]]; then
+	  	next_nodes=$(cat $graph_path | grep "$start_node" | tr ' ' '\n' | grep ":" | tr -d ':' | grep $visited_grep_args)
+	  else
+	  	echo "Unrecognised direction: $direction"
+	  	exit 1
+	  fi
 	  
 	  test $DEBUG && echo next_nodes: $next_nodes
 	  
 	  for next_node in $next_nodes; do
-	    explore_node $next_node "$visited $next_node"
+	    explore_node $next_node $stop_node $direction "$visited $next_node"
 	  done
   fi
 }
 
-explore_node "you" "" | wc -w | grep -o "[0-9]\+"
+explore_node "you" "out" $DIRECTION_FORWARD | wc -l | grep -o "[0-9]\+"
